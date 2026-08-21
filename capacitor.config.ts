fe9b15f987b665c4c0bb5aa0696e1902f1c879d2
akshatverma07 <121243421+akshatverma07@users.noreply.github.com>
2026-08-21 15:11:53 +0000

    update capacitor webDir to out folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'apinya.app',
   appName: 'apinya',
-  webDir: 'public'
+  webDir: 'out',
 };
 
 export default config;
